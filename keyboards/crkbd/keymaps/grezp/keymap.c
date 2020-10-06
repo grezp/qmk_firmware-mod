dbed3b2a823e4387dfd0d01c111faa8a6326ff08
waitwut <p.gutierrez760@gmail.com>
2020-10-05 19:02:31 -0700

    removed mouse buttons, moved del down

--- keyboards/crkbd/keymaps/grezp/keymap.c
+++ keyboards/crkbd/keymaps/grezp/keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT( \
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         _____,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,\
+         _____,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _____,\
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         _____,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   XXXXX,   XXXXX,\
+         _____,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   XXXXX,  KC_DEL,\
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
          _____,  KC_F10,   KC_F9,   KC_F8,   KC_F7,   KC_F6,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   XXXXX,   _____,\
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJ] = LAYOUT( \
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         RESET,  T_QWER,   XXXXX, KC_MS_U,   XXXXX,   XXXXX,                        XXXXX, KC_WH_D,   XXXXX,   XXXXX,  KC_F11,  KC_F12,\
+         RESET,  T_QWER,   XXXXX,   XXXXX,   XXXXX,   XXXXX,                        XXXXX,   XXXXX,   XXXXX,  KC_F11,  KC_F12,   XXXXX,\
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         XXXXX,   XXXXX, KC_MS_L, KC_MS_D, KC_MS_R,   XXXXX,                        XXXXX, KC_BTN1, KC_BTN2, KC_BTN3,   XXXXX,   XXXXX,\
+         XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,                        XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,\
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,                        XXXXX, KC_WH_U,   XXXXX,   XXXXX,   XXXXX,   XXXXX,\
+         XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,                        XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,\
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                              XXXXX,   _____,   XXXXX,      XXXXX,   _____,   XXXXX \
                                       //`--------------------------'  `--------------------------'
